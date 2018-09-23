ceae24a7f0466ff0b4e9b900c9246054990ec33d
Yugandhar Reddy Thippireddy <yreddy@Yugandhars-MacBook-Pro.local>
2018-09-23 08:40:04 -0500

    initial commit

---
+++ src/main/java/com/mulesoft/training/MuleComponent.java
@@ -0,0 +1,22 @@
+package com.mulesoft.training;
+
+import java.util.Map;
+
+public class MuleComponent {
+
+  public Map<String,String> processMap() {
+    // processMap implementation
+    return null;
+  }
+
+  public Map<String,String> processArray() {
+    // processArray implementation
+    return null;
+  }
+
+  public Map<String,String> processString() {
+    // processString implementation
+    return null;
+  }
+
+} 
